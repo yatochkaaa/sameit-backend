a3ceaf368944c474c0da2e6612a149fd39ed74c8
yatochkaaa <jaffplay@gmail.com>
2023-03-21 09:00:14 +0200

    user model

---
+++ src/users/users.model.ts
@@ -0,0 +1,39 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+type UserRoles = "admin" | "user";
+
+interface UserCreationAttrs {
+  email: string;
+  username: string;
+}
+
+@Table({ tableName: "users" })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  username: string;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    defaultValue: "user",
+  })
+  role: UserRoles;
+}
