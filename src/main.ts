6451be86a44a62b55a65f983234f041bea0586ae
yatochkaaa <jaffplay@gmail.com>
2023-03-21 08:31:59 +0200

    project created

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  const PORT = 5000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+};
+
+start();
